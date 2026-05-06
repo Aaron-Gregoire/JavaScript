a9f92bcbe846b467e0c8a923709cf6410c830a7f
Aaron-Gregoire <aaronjemmett@gmail.com>
2026-05-06 08:26:57 -0400

    test

--- lesson-1/js/guess.js
+++ lesson-1/js/guess.js
@@ -1,6 +1,7 @@
 // STEP 1: Initialize game variables
 // STEP 1a: Pick random number
 /* */
+
 // STEP 1b: Create variables to represent the three paragraphs above that will contain user feedback
 const guesses = document.querySelector('p#guesses');
 const lastResult = document.querySelector('p#lastResult');
